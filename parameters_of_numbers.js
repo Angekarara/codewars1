ed4c777ab1d01d333b66b5a68810878bef85a516
Angekarara <mbabange2020@gmail.com>
2023-08-22 23:28:06 +0200

    first commit

---
+++ parameters_of_numbers.js
@@ -0,0 +1,13 @@
+function parameter(n) {
+    //coding and coding..
+    const digits = Array.from(String(n), Number);
+      const digitSum = digits.reduce((sum, digit) => sum + digit, 0);
+      const digitProduct = digits.reduce((product, digit) => product * digit, 1);
+      
+      const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+      const lcm = (digitSum * digitProduct) / gcd(digitSum, digitProduct);
+      
+      return lcm;
+    
+  }
+  console.log(parameter(1234)); // Output: 120
