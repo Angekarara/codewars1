35a6612b5008c464abb5772535ca7a0a82aa2bcc
Angekarara <mbabange2020@gmail.com>
2023-08-26 17:08:39 +0200

    stringy strings

---
+++ stringyStrings.js
@@ -0,0 +1,10 @@
+function stringy(size) {
+    let result = '';
+  
+    for (let i = 0; i < size; i++) {
+      result += i % 2 === 0 ? '1' : '0';
+    }
+  
+    return result;
+  }
+  console.log(stringy(6));
