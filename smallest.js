7d614f9aea777e2e5fc055354ce6974ab280cabf
Angekarara <mbabange2020@gmail.com>
2023-08-29 23:05:38 +0200

    Find the smallest integer in the array

---
+++ smallest.js
@@ -0,0 +1,16 @@
+class SmallestIntegerFinder {
+    findSmallestInt(array) {
+      let smallest = array[0];
+      for (let i = 1; i < array.length; i++) {
+        if (array[i] < smallest) {
+          smallest = array[i];
+        }
+      }
+  
+      return smallest;
+    }
+  }
+  
+  const finder = new SmallestIntegerFinder();
+  
+  console.log(finder.findSmallestInt([34, -345, -1, 100]));  
