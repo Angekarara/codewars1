1bf6b520db71c2ba0692b39dd56c7004ddb6de1b
Angekarara <mbabange2020@gmail.com>
2023-08-30 22:44:15 +0200

    the office kata

---
+++ office.js
@@ -0,0 +1,19 @@
+function outed(meet, boss) {
+  meet[boss] *= 2; // Adjust boss's score (doubled)
+
+  let totalScore = 0;
+  let totalCount = 0;
+
+  for (let member in meet) {
+    totalScore += meet[member];
+    totalCount++;
+  }
+
+  const averageRating = totalScore / totalCount;
+
+  if (averageRating <= 5) {
+    return 'Get Out Now!';
+  } else {
+    return 'Nice Work Champ!';
+  }
+}
