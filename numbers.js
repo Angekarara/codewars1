f03f52e66ad7cb4c6b3a77539117559ab0f234f1
Angekarara <mbabange2020@gmail.com>
2023-08-31 21:23:41 +0200

    Numbers with this digit inside

---
+++ numbers.js
@@ -0,0 +1,19 @@
+function numbersWithDigitInside(x, d) {
+    let numbers = [];
+  
+    for (let i = 1; i <= x; i++) {
+      if (String(i).includes(String(d))) {
+        numbers.push(i);
+      }
+    }
+  
+    if (numbers.length === 0) {
+      return [0, 0, 0];
+    }
+  
+    let count = numbers.length;
+    let sum = numbers.reduce((a, b) => a + b);
+    let product = numbers.reduce((a, b) => a * b);
+  
+    return [count, sum, product];
+  }
