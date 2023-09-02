bca67114f6af8d8efe645162415efa359f50299d
Angekarara <mbabange2020@gmail.com>
2023-09-02 22:41:56 +0200

    unique in order

---
+++ uniqueOrder.js
@@ -0,0 +1,15 @@
+const  uniqueInOrder = function(iterable) {
+    let uniqueList = [];
+    let previousItem = null;
+  
+    for (let i = 0; i < iterable.length; i++) {
+      let currentItem = iterable[i];
+      
+      if (currentItem !== previousItem) {
+        uniqueList.push(currentItem);
+        previousItem = currentItem;
+      }
+    }
+  
+    return uniqueList;
+};
