e3acd65140aed924096ef4b6bbf8f0d6f5956834
Angekarara <mbabange2020@gmail.com>
2023-09-02 22:48:47 +0200

    find the position

--- findThePosition.js
+++ findThePosition.js
@@ -2,7 +2,7 @@ function position(letter) {
     letter = letter.toLowerCase();
   
     if (letter.length === 1 && /^[a-z]$/.test(letter)) {
-      var position = letter.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
+      let position = letter.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
       return "Position of alphabet: " + position;
     } else {
       return "Invalid input. Please provide a single letter from the alphabet.";
